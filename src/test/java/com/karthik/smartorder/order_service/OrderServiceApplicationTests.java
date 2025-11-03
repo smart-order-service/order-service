59c97d5425869b9b50358d7c563bf363b88d80ad
krkzero9 <46833624+krkzero9@users.noreply.github.com>
2025-11-03 09:03:31 +0530

    Initial folder structure

---
+++ src/test/java/com/karthik/smartorder/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.smartorder.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
